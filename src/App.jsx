b60a5215df7a46a4df1d639951d560f086e66c65
ragavabe <157545628+dreamingryuk@users.noreply.github.com>
2026-06-19 16:30:18 -0300

    botao de adicionar protudo

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import ListaProdutos from "./components/ListaProdutos.jsx";
 import BotaoDeletarUsuario from "./components/BotaoDeletarUsuario.jsx";
+import AdicionarProduto from "./components/AdicionarProduto.jsx";
 
 function App(){
     return(
         <div className="min-h-screen bg-gray-50 p-6">
             <h1 className="text-2xl font-bold text-center mb-6">Lista de Produtos</h1>
             <ListaProdutos />
+            <AdicionarProduto />
         </div>
     )
 }
